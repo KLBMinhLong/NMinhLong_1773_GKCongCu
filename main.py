933fc7d37895bb358430cc8e89ac7912bbbeeeae
KLBMinhLong <nguyenminhlongcntt@gmail.com>
2026-03-11 08:20:45 +0700

    sumab

---
+++ main.py
@@ -0,0 +1,9 @@
+# Nhập hai số nguyên từ người dùng
+num1 = int(input("Nhập số thứ nhất: "))
+num2 = int(input("Nhập số thứ hai: "))
+
+# Tính tổng
+tong = num1 + num2
+
+# Hiển thị kết quả
+print(f"Tổng của {num1} và {num2} là: {tong}")
